383fd498331bbff482fb7ac888c013d09c51c616
NguyenChi11 <chinguyen280200@gmail.com>
2026-04-14 13:51:48 +0700

    [update]: profile

--- template/template-parts/page/home/section-projects/index.php
+++ template/template-parts/page/home/section-projects/index.php
@@ -84,55 +84,56 @@ if ($query->have_posts()) {
 ?>
 <section class="section-portfolio" data-aos="fade-up">
     <?php if (is_customize_preview()): ?>
-        <div class="section-portfolio__hover-outline"></div>
+    <div class="section-portfolio__hover-outline"></div>
 
-        <script>
-            (function() {
-                var btn = document.querySelector('.section-portfolio__customize-button');
-                if (btn && window.parent && window.parent.wp && window.parent.wp.customize) {
-                    btn.addEventListener('click', function() {
-                        window.parent.wp.customize.section('buildpro_portfolio_section').focus();
-                    });
-                }
-            })();
-        </script>
+    <script>
+    (function() {
+        var btn = document.querySelector('.section-portfolio__customize-button');
+        if (btn && window.parent && window.parent.wp && window.parent.wp.customize) {
+            btn.addEventListener('click', function() {
+                window.parent.wp.customize.section('buildpro_portfolio_section').focus();
+            });
+        }
+    })();
+    </script>
     <?php endif; ?>
     <div class="section-portfolio__header">
         <?php if ($portfolio_title !== ''): ?>
-            <h2 class="section-portfolio__title"><?php echo esc_html($portfolio_title); ?></h2>
+        <h2 class="section-portfolio__title"><?php echo esc_html($portfolio_title); ?></h2>
         <?php endif; ?>
         <?php if ($portfolio_desc !== ''): ?>
-            <p class="section-portfolio__description"><?php echo esc_html($portfolio_desc); ?></p>
+        <p class="section-portfolio__description"><?php echo esc_html($portfolio_desc); ?></p>
         <?php endif; ?>
     </div>
     <div class="swiper section-portfolio__swiper">
         <div class="swiper-wrapper">
             <?php foreach ($portfolio_items as $item): ?>
-                <div class="swiper-slide">
-                    <a class="section-portfolio__item" href="<?php echo esc_url($item['link_url']); ?>"
-                        <?php echo $item['link_target'] ? 'target="' . esc_attr($item['link_target']) . '"' : ''; ?>>
-                        <div class="section-portfolio__item-image">
-                            <?php
+            <div class="swiper-slide">
+                <a class="section-portfolio__item" href="<?php echo esc_url($item['link_url']); ?>"
+                    <?php echo $item['link_target'] ? 'target="' . esc_attr($item['link_target']) . '"' : ''; ?>>
+                    <div class="section-portfolio__item-image">
+                        <?php
                             $img_url = $item['image_id'] ? wp_get_attachment_image_url($item['image_id'], 'full') : '';
                             ?>
-                            <?php if ($img_url): ?>
-                                <div class="section-portfolio__item-bg"
-                                    style="background-image: url('<?php echo esc_url($img_url); ?>');"></div>
-                            <?php else: ?>
-                                <div class="section-portfolio__item-bg"></div>
-                            <?php endif; ?>
+                        <?php if ($img_url): ?>
+                        <div class="section-portfolio__item-bg"
+                            style="background-image: url('<?php echo esc_url($img_url); ?>');"></div>
+                        <?php else: ?>
+                        <div class="section-portfolio__item-bg"></div>
+                        <?php endif; ?>
+                    </div>
+                    <div class="section-portfolio__item-content">
+                        <p class="section-portfolio__item-text"><?php echo $item['text']; ?></p>
+                        <h3 class="section-portfolio__item-name"><?php echo $item['name']; ?></h3>
+                        <div class="section-portfolio__item-location-wrapper">
+                            <img src="<?php echo esc_url(get_theme_file_uri('/assets/images/icon/icon_location.png')); ?>"
+                                alt="<?php echo esc_attr__('Location', 'buildpro'); ?>"
+                                class="section-portfolio__item-location-icon">
+                            <p class="section-portfolio__item-location"><?php echo esc_html($item['location']); ?></p>
                         </div>
-                        <div class="section-portfolio__item-content">
-                            <p class="section-portfolio__item-text"><?php echo $item['text']; ?></p>
-                            <h3 class="section-portfolio__item-name"><?php echo $item['name']; ?></h3>
-                            <div class="section-portfolio__item-location-wrapper">
-                                <img src="<?php echo esc_url(get_theme_file_uri('/assets/images/icon/icon_location.png')); ?>"
-                                    alt="<?php echo esc_attr__('Location', 'buildpro'); ?>" class="section-portfolio__item-location-icon">
-                                <p class="section-portfolio__item-location"><?php echo esc_html($item['location']); ?></p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                    </div>
+                </a>
+            </div>
             <?php endforeach; ?>
         </div>
         <div class="swiper-button-prev section-portfolio__swiper-prev"></div>
@@ -149,8 +150,11 @@ if ($query->have_posts()) {
         <a class="section-portfolio__page-link-text" href="<?php echo esc_url($projects_page_url); ?>">
             <?php echo esc_html($portfolio_view_all_text); ?>
         </a>
-        <img class="section-banner__item-button-icon"
-            src="<?php echo esc_url(get_theme_file_uri('/assets/images/icon/Arrow_Right.png')); ?>" alt="<?php echo esc_attr__('Right arrow', 'buildpro'); ?>">
+        <svg class="section-banner__item-button-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"
+            aria-hidden="true" focusable="false">
+            <path
+                d="M566.6 342.6C579.1 330.1 579.1 309.8 566.6 297.3L406.6 137.3C394.1 124.8 373.8 124.8 361.3 137.3C348.8 149.8 348.8 170.1 361.3 182.6L466.7 288L96 288C78.3 288 64 302.3 64 320C64 337.7 78.3 352 96 352L466.7 352L361.3 457.4C348.8 469.9 348.8 490.2 361.3 502.7C373.8 515.2 394.1 515.2 406.6 502.7L566.6 342.7z" />
+        </svg>
     </div>
     <?php if (empty($portfolio_items)) {
         return;
